2a5477c87afd2b4151fa8ad18f7db0a6d2220550
hurric <1520386112@qq.com>
2025-06-29 14:34:49 +0800

    set gray 接口

--- Assets/Scripts/NPC/NPC_Interaction.cs
+++ Assets/Scripts/NPC/NPC_Interaction.cs
@@ -14,13 +14,14 @@ public class NPC_Interaction : MonoBehaviour
 
     public NPC_BehaviorCtrl npc_BehaviorCtrl;
     public PlayerCtrl playerCtrl;
-   
+    private Material _material;
 
     private void Awake()
     {
         if (npc_BehaviorCtrl == null)
         {
             Reset(); // 如果没有手动设置npc_BehaviorCtrl，则尝试从父物体获取
+            _material =  npc_BehaviorCtrl.GetComponent<Renderer>().material;
         }
     }
 
@@ -299,6 +300,51 @@ public class NPC_Interaction : MonoBehaviour
             Debug.Log($"结束与NPC {npc_BehaviorCtrl.npcID} 的交互");
             GameObject.FindWithTag("INTERACT").SetActive(false);
             // 可以在这里调用其他方法来处理具体的交互结束逻辑
+        }
+    }
+    
+    public void SetGray()
+    {
+        StopAllCoroutines();
+        StartCoroutine(ToGrayCo());
+    }
+
+    public void SetColorful()
+    {
+        StopAllCoroutines();
+        StartCoroutine(ToColorfulCo());
+    }
+    
+    private IEnumerator ToGrayCo()
+    {
+        float value = _material.GetFloat("_Value");
+        _material.SetFloat("_Value", 1);
+        while (value > 0 + 1e-3)
+        {
+            value -= Time.deltaTime * 2;
+            if (value <= 0)
+            {
+                value = 0;
+            }
+            _material.SetFloat("_Value", value);
+            yield return null;
+        }
+    }
+    
+    private IEnumerator ToColorfulCo()
+    {
+        StopAllCoroutines();
+        float value = _material.GetFloat("_Value");
+        _material.SetFloat("_Value", 1);
+        while (value < 1 - 1e-3)
+        {
+            value += Time.deltaTime * 2;
+            if (value <= 0)
+            {
+                value = 0;
+            }
+            _material.SetFloat("_Value", value);
+            yield return null;
         }
     }
 }
