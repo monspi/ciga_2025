24e9c93f7a222fd6af757668a5db78bee80db5dd
hurric <1520386112@qq.com>
2025-06-29 14:09:46 +0800

    set gray interface

--- Assets/Scripts/NPC/NPC_Interaction.cs
+++ Assets/Scripts/NPC/NPC_Interaction.cs
@@ -9,12 +9,14 @@ using UnityEngine;
 public class NPC_Interaction : MonoBehaviour
 {
     public NPC_BehaviorCtrl npc_BehaviorCtrl;
+    private Material _material;
 
     private void Awake()
     {
         if(npc_BehaviorCtrl == null)
         {
             Reset(); // ���û���ֶ�����npc_BehaviorCtrl�����ԴӸ������ȡ
+            _material =  npc_BehaviorCtrl.GetComponent<Renderer>().material;
         }
     }
 
@@ -51,6 +53,51 @@ public class NPC_Interaction : MonoBehaviour
             // ����NPC�����߼�
             Debug.Log($"������NPC {npc_BehaviorCtrl.npcID} �Ľ���");
             // ������������������������������Ľ��������߼�
+        }
+    }
+
+    public void SetGray()
+    {
+        StopAllCoroutines();
+        StartCoroutine(ToGrayCo());
+    }
+
+    public void SetColorful()
+    {
+        StopAllCoroutines();
+        StartCoroutine(ToColorfulCo());
+    }
+    
+    private IEnumerator ToGrayCo()
+    {
+        float value = _material.GetFloat("_Value");
+        _material.SetFloat("_Value", 1);
+        while (value > 0 + 1e-3)
+        {
+            value -= Time.deltaTime * 2;
+            if (value <= 0)
+            {
+                value = 0;
+            }
+            _material.SetFloat("_Value", value);
+            yield return null;
+        }
+    }
+    
+    private IEnumerator ToColorfulCo()
+    {
+        StopAllCoroutines();
+        float value = _material.GetFloat("_Value");
+        _material.SetFloat("_Value", 1);
+        while (value < 1 - 1e-3)
+        {
+            value += Time.deltaTime * 2;
+            if (value <= 0)
+            {
+                value = 0;
+            }
+            _material.SetFloat("_Value", value);
+            yield return null;
         }
     }
 }
